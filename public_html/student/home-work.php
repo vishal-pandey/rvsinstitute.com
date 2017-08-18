d864a8c9666290eac9c5c47ed662e525c95de768
vishal-pandey <pandeyvishal25@yahoo.com>
2017-08-18 22:50:26 +0530

    18/08/2017

--- public_html/student/home-work.php
+++ public_html/student/home-work.php
@@ -1,8 +1,11 @@
+<?php
+	$sql = "SELECT * FROM pages where pid = 'home-work'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+?>
 <div class="home-work">
 	<h3 align="center" style="margin-top: 0px; color: green;"><u>Home Work</u></h3>
 	<div class=" col-sm-12">
-		<a href="#"><button class="btn btn-primary pull-right">Download</button></a>
-		<b>HomeWork For Class : </b><span class="s-class">10th</span><br>
-		<b>Date : </b><span class="h-date">20/10/2017</span>
+		<?php echo $row['content']; ?>
 	</div>
 </div>
