3f89055ee6415c5305332b42d21e8ebf1a0d9120
vishal-pandey <pandeyvishal25@yahoo.com>
2017-09-24 23:08:01 +0530

    24-09-2017

--- public_html/student/pay-fee.php
+++ public_html/student/pay-fee.php
@@ -83,7 +83,6 @@
   </script>
   </head>
   <body onload="submitPayuForm()">
-    <h2>PayU Form</h2>
     <br/>
     <?php if($formError) { ?>
 	
@@ -95,96 +94,36 @@
       <input type="hidden" name="key" value="<?php echo $MERCHANT_KEY ?>" />
       <input type="hidden" name="hash" value="<?php echo $hash ?>"/>
       <input type="hidden" name="txnid" value="<?php echo $txnid ?>" />
-      <table>
-        <tr>
-          <td><b>Mandatory Parameters</b></td>
-        </tr>
-        <tr>
-          <td>Amount: </td>
-          <td><input name="amount" value="<?php echo (empty($posted['amount'])) ? '' : $posted['amount'] ?>" /></td>
-          <td>First Name: </td>
-          <td><input name="firstname" id="firstname" value="<?php echo (empty($posted['firstname'])) ? '' : $posted['firstname']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Email: </td>
-          <td><input name="email" id="email" value="<?php echo (empty($posted['email'])) ? '' : $posted['email']; ?>" /></td>
-          <td>Phone: </td>
-          <td><input name="phone" value="<?php echo (empty($posted['phone'])) ? '' : $posted['phone']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Product Info: </td>
-          <td colspan="3"><textarea name="productinfo"><?php echo (empty($posted['productinfo'])) ? '' : $posted['productinfo'] ?></textarea></td>
-        </tr>
-        <tr>
-          <td>Success URI: </td>
-          <td colspan="3"><input name="surl" value="<?php echo (empty($posted['surl'])) ? '' : $posted['surl'] ?>" size="64" /></td>
-        </tr>
-        <tr>
-          <td>Failure URI: </td>
-          <td colspan="3"><input name="furl" value="<?php echo (empty($posted['furl'])) ? '' : $posted['furl'] ?>" size="64" /></td>
-        </tr>
+      
+          <input name="amount" type="hidden" value="<?php echo $profile['thefees'];?>" />
 
-        <tr>
-          <td colspan="3"><input type="hidden" name="service_provider" value="payu_paisa" size="64" /></td>
-        </tr>
+          <input name="firstname" id="firstname"  type="hidden" value="<?php echo $profile['s_name'];?>" />
+        
+        
 
-        <tr>
-          <td><b>Optional Parameters</b></td>
-        </tr>
-        <tr>
-          <td>Last Name: </td>
-          <td><input name="lastname" id="lastname" value="<?php echo (empty($posted['lastname'])) ? '' : $posted['lastname']; ?>" /></td>
-          <td>Cancel URI: </td>
-          <td><input name="curl" value="" /></td>
-        </tr>
-        <tr>
-          <td>Address1: </td>
-          <td><input name="address1" value="<?php echo (empty($posted['address1'])) ? '' : $posted['address1']; ?>" /></td>
-          <td>Address2: </td>
-          <td><input name="address2" value="<?php echo (empty($posted['address2'])) ? '' : $posted['address2']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>City: </td>
-          <td><input name="city" value="<?php echo (empty($posted['city'])) ? '' : $posted['city']; ?>" /></td>
-          <td>State: </td>
-          <td><input name="state" value="<?php echo (empty($posted['state'])) ? '' : $posted['state']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Country: </td>
-          <td><input name="country" value="<?php echo (empty($posted['country'])) ? '' : $posted['country']; ?>" /></td>
-          <td>Zipcode: </td>
-          <td><input name="zipcode" value="<?php echo (empty($posted['zipcode'])) ? '' : $posted['zipcode']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>UDF1: </td>
-          <td><input name="udf1" value="<?php echo (empty($posted['udf1'])) ? '' : $posted['udf1']; ?>" /></td>
-          <td>UDF2: </td>
-          <td><input name="udf2" value="<?php echo (empty($posted['udf2'])) ? '' : $posted['udf2']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>UDF3: </td>
-          <td><input name="udf3" value="<?php echo (empty($posted['udf3'])) ? '' : $posted['udf3']; ?>" /></td>
-          <td>UDF4: </td>
-          <td><input name="udf4" value="<?php echo (empty($posted['udf4'])) ? '' : $posted['udf4']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>UDF5: </td>
-          <td><input name="udf5" value="<?php echo (empty($posted['udf5'])) ? '' : $posted['udf5']; ?>" /></td>
-          <td>PG: </td>
-          <td><input name="pg" value="<?php echo (empty($posted['pg'])) ? '' : $posted['pg']; ?>" /></td>
-        </tr>
-        <tr>
+          <input name="email" id="email" type="hidden" value="<?php echo $profile['s_email'];?>" />
+
+          <input name="phone" type="hidden" value="<?php echo $profile['s_mobile'];?>" />
+        
+        
+
+          <textarea name="productinfo" style="display: none;">Payment of Academic Fee of Rs <?php echo $profile['thefees']; ?>  for <?php echo $profile['s_name']; ?> </textarea>
+        
+        
+          <input name="surl" type="hidden" value="http://www.rvsinstitute.com/test/pay/success.php" size="64" />
+        
+        
+          <input name="furl" type="hidden" value="http://www.rvsinstitute.com/test/pay/failure.php" size="64" />
+        
+
+        
+          <input type="hidden" name="service_provider" value="payu_paisa" size="64" />
+        
+
+        
           <?php if(!$hash) { ?>
-            <td colspan="4"><input type="submit" value="Submit" /></td>
-          <?php } ?>
-        </tr>
-      </table>
+            <center><input type="submit" class="btn btn-primary" value="Pay Fees <?php echo $profile['thefees'];?> Rs" />
+          <?php } ?></center>
     </form>
   </body>
 </html>
-
-<div class="pay-fee col-sm-12">
-	<center><button class="btn btn-primary"> Pay Fees <?php
-		echo $profile['thefees'];
-	?> Rs</button></center>
-</div>
