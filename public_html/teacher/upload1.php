f4345213ff70325c1951204a397942b1ddf528b3
vishal-pandey <pandeyvishal25@yahoo.com>
2017-09-28 14:38:33 +0530

    28/09/2017

--- public_html/teacher/upload1.php
+++ public_html/teacher/upload1.php
@@ -1,15 +1,15 @@
 <?php
-	// $t_uid = $_POST['t_uid'];
- //    $target_dir = "../tpic/";
- //    $target_file = $target_dir . basename($_FILES["t_pic"]["name"]);
- //    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
- //    move_uploaded_file($_FILES["t_pic"]["tmp_name"], $target_file);
-    // echo "success";
+	$t_uid = $_POST['t_uid'];
+    $target_dir = "../tpic/";
+    $target_file = $target_dir . basename($_FILES["t_pic"]["name"]);
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    move_uploaded_file($_FILES["t_pic"]["tmp_name"], $target_file);
+    echo "success";
 ?>
 <?php
-$target_dir = "../tpic/";
-$target_file = $target_dir . $_POST['t_pic'] . ".jpg";
+// $target_dir = "../tpic/";
+// $target_file = $target_dir . $_POST['t_pic'] . ".jpg";
 
-move_uploaded_file($_FILES["tpic"]["tmp_name"], $target_file)
-echo "success";
+// move_uploaded_file($_FILES["tpic"]["tmp_name"], $target_file)
+// echo "success";
 ?>
