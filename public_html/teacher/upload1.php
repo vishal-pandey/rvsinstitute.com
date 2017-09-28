33826f6f5095cdc4bee58c7f25ff4f89501af61e
vishal-pandey <pandeyvishal25@yahoo.com>
2017-09-28 14:40:21 +0530

    28/09/2017

--- public_html/teacher/upload1.php
+++ public_html/teacher/upload1.php
@@ -1,7 +1,8 @@
 <?php
 	$t_uid = $_POST['t_uid'];
     $target_dir = "../tpic/";
-    $target_file = $target_dir . basename($_FILES["t_pic"]["name"]);
+    // $target_file = $target_dir . basename($_FILES["t_pic"]["name"]);
+    $target_file = $target_dir . $t_uid.".jpg";
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     move_uploaded_file($_FILES["t_pic"]["tmp_name"], $target_file);
     echo "success";
