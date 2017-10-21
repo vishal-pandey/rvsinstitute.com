106d8bea882a7db0d91278826d38c672ee263178
vishal-pandey <pandeyvishal25@yahoo.com>
2017-10-21 08:25:32 +0530

    security

--- public_html/admin/post/fees/fees-form.php
+++ public_html/admin/post/fees/fees-form.php
@@ -1,5 +1,5 @@
 <?php
-		if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) 
+		if ( $_SERVER['REQUEST_METHOD']=='POST' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) 
 		{
 			header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
 		}
