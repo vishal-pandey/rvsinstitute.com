28800cdc4a6442d5086ddde4652ceae1f940eac2
vishal-pandey <pandeyvishal25@yahoo.com>
2017-10-21 08:23:47 +0530

    security

--- public_html/admin/post/fees/fees-form.php
+++ public_html/admin/post/fees/fees-form.php
@@ -1,17 +1,23 @@
 <?php
-	include_once "../../../../adminlibrary/phplib.php";
-	$thefees = $_POST['thefees'];
-	$s_mobile = $_POST['s_mobile'];
-
-	if($_POST['check'] == "thefees"){
-		$sql = "update r_student set thefees = {$thefees} where s_mobile = {$s_mobile}";
-		$conn->query($sql);
-	}
-
-	if($_POST['check'] == "s_date"){
-		$s_date=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
-		$sql = "update r_student set s_date = '{$s_date}' where s_mobile = {$s_mobile}";
-		$conn->query($sql);
-	}
+		if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) 
+		{
+			header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+		}
+		else{
+			include_once "../../../../adminlibrary/phplib.php";
+			$thefees = $_POST['thefees'];
+			$s_mobile = $_POST['s_mobile'];
+	
+			if($_POST['check'] == "thefees"){
+				$sql = "update r_student set thefees = {$thefees} where s_mobile = {$s_mobile}";
+				$conn->query($sql);
+			}
+	
+			if($_POST['check'] == "s_date"){
+				$s_date=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
+				$sql = "update r_student set s_date = '{$s_date}' where s_mobile = {$s_mobile}";
+				$conn->query($sql);
+			}
+		}
 ?>
 success
